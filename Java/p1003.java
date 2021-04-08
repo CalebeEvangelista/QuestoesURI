a1209a7ca45df98beb5ca84dd38571aa8eda63bd
Calebe Evangelista <calebeevangelista@CalebePC.CalebeCORP>
2021-04-08 15:17:55 -0300

    Adicionando o p1003

--- Java/p1003.java
+++ Java/p1003.java
@@ -1,5 +1,18 @@
 package Java;
+import java.util.Scanner;
 
 public class p1003 {
+	public static void main (String[] args) {
+	Scanner garotinho = new Scanner(System.in);
+	
+	int A,B,SOMA;
+	
+	A = garotinho.nextInt();
+	B = garotinho.nextInt();
+	SOMA = A + B;
+	
+	System.out.println("SOMA = " + SOMA);
 
+	garotinho.close();
+}
 }
